9df7dfa7c9d9b051bc69dedf772aaae060c4f5e6
Alex08080 <alexmoderan@gmail.com>
2026-01-19 02:26:47 +0100

    Parsing des commandes

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 17:32:00 by alex              #+#    #+#             */
-/*   Updated: 2026/01/17 17:32:36 by alex             ###   ########.fr       */
+/*   Updated: 2026/01/19 02:12:31 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,6 @@
 # include "../libft/ft_printf.h"
 # include <stdio.h>
 
+char	*path_to_find(char *cmd, char **envp);
+void	free_split(char **tab);
 #endif 
